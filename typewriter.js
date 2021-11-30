00c131cf216f2b5a3e4d7840e38b2a98b93b147d
houseofsam <samxsaid@gmail.com>
2021-11-30 13:04:34 +0000

    print to console w/ 50ms delay between each character & add newline character at end of animation

---
+++ typewriter.js
@@ -0,0 +1,15 @@
+const sentence = "hello there from lighthouse labs";
+
+let time = 0;
+
+for (let i = 0; i < sentence.length; i++) {
+  time += 50;
+  setTimeout(() => process.stdout.write(sentence[i]), time);
+}
+
+setTimeout(() => process.stdout.write('\n'), time);
+
+
+// setTimeout(() => {
+
+// }, 1000);
